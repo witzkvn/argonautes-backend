784f49aa01147c6c92ee47f55c2e48623b86ed60
witzkvn <witz.kevin05@gmail.com>
2022-07-07 16:32:51 +0200

    added connection to mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
+
+const DB = process.env.DB.replace("<password>", process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB)
+  .then(() =>
+    console.log("Connection with DB done. Listening for requests...")
+  );
 
 const port = process.env.PORT || 3001;
 const server = app.listen(port, () => {
