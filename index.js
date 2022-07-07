601b24d1a51a4e8def1394478846b0b07af35573
witzkvn <witz.kevin05@gmail.com>
2022-07-07 18:04:33 +0200

    gestion des erreur unhandled

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+// pour les exceptions
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught exception. Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1); // fermer toute l'app
+});
+
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
@@ -16,4 +23,24 @@ mongoose
 const port = process.env.PORT || 3001;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+// pour les promesses rejetées
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled rejection. Shutting down...");
+  console.log(err.name, err.messager);
+  server.close(() => {
+    // fermer serveur pour finir les requêtes en cours
+    process.exit(1); // fermer toute l'app
+  });
+});
+
+// termination signal
+process.on("SIGTERM", () => {
+  console.log("SIGTERM RECEIVED. Shutting down...");
+  server.close(() => {
+    // fermer serveur pour finir les requêtes en cours
+    console.log("Process terminated!");
+    process.exit(1); // fermer toute l'app
+  });
 });
